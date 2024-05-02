a653bd982a0757c373dafa69519f465ecda8775a
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-05-02 19:51:53 +0000

    work in prog, task 2 done

---
+++ react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,48 @@
+import './App.css';
+import logo from './holberton-logo.jpg';
+import { getFullYear, getFooterCopy } from "./utils";
+
+function App() {
+  const isIndex = true;
+
+  const handleLableClick = (id) => {
+    const inputElement = document.getElementById(id);
+    if (inputElement) {
+      inputElement.focus();
+      }
+    };
+  
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} alt="logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>
+          Login to access the full dashboard
+        </p>
+        {/* Labels and Inputs for Email and Password */}
+        <div>
+          <label htmlFor="email" onClick={() => handleLableClick('email')}>
+            Email:
+          </label>
+          <input type="email" id="email" />
+        </div>
+        <div>
+          <label htmlFor="password" onClick={() => handleLableClick("password")}>
+            Password:
+          </label>
+          <input type="password" id="password"/>
+        </div>
+        {/* OK Button */}
+        <button type="button">OK</button>
+      </div>
+      <div className="App-footer">
+      <p>Copyright {getFullYear()} - {getFooterCopy(isIndex)}</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
