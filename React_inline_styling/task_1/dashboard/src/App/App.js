49fb9214a3b8e78b12f12e6f8f2b5a735f714fec
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-05-17 00:18:39 +0000

    task_1 done finally

--- React_inline_styling/task_1/dashboard/src/App/App.js
+++ React_inline_styling/task_1/dashboard/src/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
-import { StyleSheet, css } from 'aphrodite'; 
+import { StyleSheet, css } from 'aphrodite';
 import Header from '../Header/Header';
 import Login from '../Login/Login';
 import Footer from '../Footer/Footer';
@@ -9,30 +9,32 @@ import CourseList from '../CourseList/CourseList';
 import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
 import BodySection from '../BodySection/BodySection';
 
-
 const styles = StyleSheet.create({
   app: {
     position: 'relative',
     minHeight: '100vh',
   },
-  Header: {
+  header: {
     display: "flex",
     flexDirection: "row-reverse",
     width: "100%",
     borderBottom: `3px solid #e1354b`,
     justifyContent: "space-between",
   },
+  body: {
+    maxWidth: '90%',
+    margin: '0 auto',
+  },
   footer: {
-	  backgroundColor: '#fff',
-	  textAlign: 'center',
-	  width: '100%',
-	  bottom: '0px',
-	  borderTop: '3px solid #e1354b',
-	  fontStyle: 'italic',
-	  padding: '1rem 0'
-  }
+    backgroundColor: '#fff',
+    textAlign: 'center',
+    width: '100%',
+    bottom: '0px',
+    borderTop: '3px solid #e1354b',
+    fontStyle: 'italic',
+    padding: '1rem 0',
+  },
 });
-
 
 class App extends Component {
   componentDidMount() {
@@ -52,26 +54,27 @@ class App extends Component {
   }
 
   render() {
-      const { isLoggedIn } = this.props;
-      const listCourses = [
-        { id: 1, name: 'ES6', credit: 60 },
-        { id: 2, name: 'Webpack', credit: 20 },
-        { id: 3, name: 'React', credit: 40 },
-      ];
-  
-      const listNotifications = [
-        { id: 1, type: 'default', value: "New course available" },
-        { id: 2, type: 'urgent', value: "New resume available" },
-        { id: 3, type: 'urgent', html: { __html: '<strong>Urgent requirement</strong> - complete by EOD' } }
-      ];
-  
-      return (
-        <Fragment>
-          <div className={css(styles.app)}>
-          <div className={css(styles.Header)}>
-          <Notifications listNotifications={listNotifications} /> {/*the bottom one should be right. not sure why there is two. this needs to be here for tests. to afraid to mess with it.*/}
+    const { isLoggedIn } = this.props;
+    const listCourses = [
+      { id: 1, name: 'ES6', credit: 60 },
+      { id: 2, name: 'Webpack', credit: 20 },
+      { id: 3, name: 'React', credit: 40 },
+    ];
+
+    const listNotifications = [
+      { id: 1, type: 'default', value: "New course available" },
+      { id: 2, type: 'urgent', value: "New resume available" },
+      { id: 3, type: 'urgent', html: { __html: '<strong>Urgent requirement</strong> - complete by EOD' } }
+    ];
+
+    return (
+      <Fragment>
+        <div className={css(styles.app)}>
+          <div className={css(styles.header)}>
+            <Notifications listNotifications={listNotifications} />
             <Header />
-            </div>
+          </div>
+          <div className={css(styles.body)}>
             {isLoggedIn ? (
               <BodySectionWithMarginBottom title="Course list">
                 <CourseList listCourses={listCourses} />
@@ -84,14 +87,15 @@ class App extends Component {
             <BodySection title="News from the School">
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.</p>
             </BodySection>
-            <div className={css(styles.footer)}>
-              </div>
+          </div>
+          <div className={css(styles.footer)}>
             <Footer />
           </div>
-        </Fragment>
-      );
-    };
-  };
+        </div>
+      </Fragment>
+    );
+  }
+}
 
 App.propTypes = {
   isLoggedIn: PropTypes.bool,
