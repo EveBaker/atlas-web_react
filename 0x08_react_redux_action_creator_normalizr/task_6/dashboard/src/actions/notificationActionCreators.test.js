0e34556ee10493232ac52da0a715f0162b82f4e0
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-05-22 19:31:26 +0000

    task 6 done

---
+++ 0x08_react_redux_action_creator_normalizr/task_6/dashboard/src/actions/notificationActionCreators.test.js
@@ -0,0 +1,21 @@
+import { MARK_AS_READ, SET_TYPE_FILTER } from './notificationActionTypes';
+import { markAsRead, setNotificationFilter } from './notificationActionCreators';
+import { NotificationTypeFilters } from './notificationActionTypes';
+
+describe('Notification action creators', () => {
+  it('markAsRead should create an action to mark a notification as read', () => {
+    const expectedAction = {
+      type: MARK_AS_READ,
+      index: 1,
+    };
+    expect(markAsRead(1)).toEqual(expectedAction);
+  });
+
+  it('setNotificationFilter should create an action to set the notification filter', () => {
+    const expectedAction = {
+      type: SET_TYPE_FILTER,
+      filter: NotificationTypeFilters.DEFAULT,
+    };
+    expect(setNotificationFilter(NotificationTypeFilters.DEFAULT)).toEqual(expectedAction);
+  });
+});
