13cd302c39d5ebbc754b297235c0e5b3286f33b0
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-06-05 00:39:18 +0000

    task 0-1 done

--- react_redux_connectors_and_providers/task_0/dashboard/src/index.js
+++ react_redux_connectors_and_providers/task_0/dashboard/src/index.js
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App/App';
 import Notifications from "./Notifications/Notifications";
+import uiReducer from './reducers/uiReducer';
+
+const store = createStore(uiReducer);
 
 ReactDOM.render(
   <React.StrictMode>
+     <Provider store={store}>
   <div className="root-notifications">
     <Notifications />
   </div>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
